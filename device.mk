27328e8a0e3def19bb0722a78f4e1f708b5bcaa1
Nope Nope <kingdutchisbad+12@proton.me>
2024-04-09 20:46:55 +0530

    twolip: Bump USB Basic to 1.3

--- device.mk
+++ device.mk
@@ -67,7 +67,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.basic
 
 # Wifi
 PRODUCT_PACKAGES += \
